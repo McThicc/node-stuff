c676e159db4dc829d74f28f236e72d8c8a8a980e
Steven Jones <104644149+McThicc@users.noreply.github.com>
2024-05-27 16:30:27 -0400

    Comments are stupid

--- server.js
+++ server.js
@@ -7,14 +7,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000');
 })
-
-// local host is like a domain name
-// takes us to a loop back ip address
-// 127.0.0.1
-// points directly back to the computer
-
-// port numbers are like doors into a computer
-// every application uses a different port number
-// our server needs its own port number
-// 3000 is popular for local web development
-// localhost:3000
